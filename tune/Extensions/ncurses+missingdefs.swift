9e9cf8bbc2a6ebe86c63329b5cae5c504f39cb83
Bruno Philipe <brunophilipe@me.com>
2017-04-29 04:44:07 +0200

    Adds missing ncurses definitions

---
+++ tune/Extensions/ncurses+missingdefs.swift
@@ -0,0 +1,30 @@
+//
+//  ncurses+missingdefs.swift
+//  tune
+//
+//  Created by Bruno Philipe on 29/4/17.
+//  Copyright © 2017 Bruno Philipe. All rights reserved.
+//
+
+import Foundation
+
+func NCURSES_BITS(_ mask: Int32, _ shift: Int32) -> Int32
+{
+	return ((mask) << ((shift) + NCURSES_ATTR_SHIFT))
+}
+
+let A_STANDOUT			= NCURSES_BITS(1,8)
+let A_UNDERLINE			= NCURSES_BITS(1,9)
+let A_REVERSE			= NCURSES_BITS(1,10)
+let A_BLINK				= NCURSES_BITS(1,11)
+let A_DIM				= NCURSES_BITS(1,12)
+let A_BOLD				= NCURSES_BITS(1,13)
+let A_ALTCHARSET		= NCURSES_BITS(1,14)
+let A_INVIS				= NCURSES_BITS(1,15)
+let A_PROTECT			= NCURSES_BITS(1,16)
+let A_HORIZONTAL		= NCURSES_BITS(1,17)
+let A_LEFT				= NCURSES_BITS(1,18)
+let A_LOW				= NCURSES_BITS(1,19)
+let A_RIGHT				= NCURSES_BITS(1,20)
+let A_TOP				= NCURSES_BITS(1,21)
+let A_VERTICAL			= NCURSES_BITS(1,22)
